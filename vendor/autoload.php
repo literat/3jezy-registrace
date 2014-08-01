58b5455e8e6f136fe292efe168bd7ef682c597f0
literat <tomaslitera@hotmail.com>
2014-08-01 11:15:28 +0200

    Add SkautIS library

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInite2f5c4a1bb156bf191fbedad69e1639b::getLoader();
+return ComposerAutoloaderInitbeb0a751bd3c0304777144642d1610fb::getLoader();
