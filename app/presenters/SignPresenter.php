4b535632692c35604ec210ddf075d254f6276383
literat <tomaslitera@hotmail.com>
2015-01-19 22:12:59 +0100

    Prepare Sign In page
      * sign in wtih mail and password
      * sign up for public
      * sign in with SkautIS

--- app/presenters/SignPresenter.php
+++ app/presenters/SignPresenter.php
@@ -20,15 +20,20 @@ class SignPresenter extends BasePresenter
 	protected function createComponentSignInForm()
 	{
 		$form = new Nette\Application\UI\Form;
-		$form->addText('username', 'Username:')
-			->setRequired('Please enter your username.');
+		$form->addText('username', 'E-mail:')
+			->setRequired('Please enter your e-mail.')
+			->setAttribute('class', 'form-control')
+			->setAttribute('placeholder', 'E-mail');
 
 		$form->addPassword('password', 'Password:')
-			->setRequired('Please enter your password.');
+			->setRequired('Please enter your password.')
+			->setAttribute('class', 'form-control')
+			->setAttribute('placeholder', 'Password');
 
 		$form->addCheckbox('remember', 'Keep me signed in');
 
-		$form->addSubmit('send', 'Sign in');
+		$form->addSubmit('signin', 'Sign in')
+			->setAttribute('class', 'btn btn-lg btn-primary btn-block');
 
 		// call method signInFormSucceeded() on success
 		$form->onSuccess[] = $this->signInFormSucceeded;
@@ -37,6 +42,60 @@ class SignPresenter extends BasePresenter
 
 
 	public function signInFormSucceeded($form, $values)
+	{
+		if ($values->remember) {
+			$this->getUser()->setExpiration('14 days', FALSE);
+		} else {
+			$this->getUser()->setExpiration('20 minutes', TRUE);
+		}
+
+		try {
+			$this->getUser()->login($values->username, $values->password);
+			$this->redirect('Homepage:');
+
+		} catch (Nette\Security\AuthenticationException $e) {
+			$form->addError($e->getMessage());
+		}
+	}
+
+
+	/**
+	 * Registration form factory.
+	 * @return Nette\Application\UI\Form
+	 */
+	protected function createComponentSignUpForm()
+	{
+		$form = new Nette\Application\UI\Form;
+		$form->addText('name', 'Name:')
+			->setRequired('Please enter your name.')
+			->setAttribute('class', 'form-control')
+			->setAttribute('placeholder', 'Name');
+
+		$form->addText('surname', 'Surname:')
+			->setRequired('Please enter your surname.')
+			->setAttribute('class', 'form-control')
+			->setAttribute('placeholder', 'Surname');
+
+		$form->addText('email', 'E-mail:')
+			->setRequired('Please enter your e-mail.')
+			->setAttribute('class', 'form-control')
+			->setAttribute('placeholder', 'E-mail');
+
+		$form->addPassword('password', 'Password:')
+			->setRequired('Please enter your surname.')
+			->setAttribute('class', 'form-control')
+			->setAttribute('placeholder', 'Password');
+
+		$form->addSubmit('signup', 'Sign up')
+			->setAttribute('class', 'btn btn-lg btn-success btn-block');
+
+		// call method signInFormSucceeded() on success
+		$form->onSuccess[] = $this->signUpFormSucceeded;
+		return $form;
+	}
+
+
+	public function signUpFormSucceeded($form, $values)
 	{
 		if ($values->remember) {
 			$this->getUser()->setExpiration('14 days', FALSE);
