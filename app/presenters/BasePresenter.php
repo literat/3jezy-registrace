f559259d0ff4593b4cb22d6f9617ee595c49d5dc
literat <tomaslitera@hotmail.com>
2015-01-25 20:23:17 +0100

    Basic presenters
      * base presenter as default
      * base secured presenter for all pages that must be behind login form

--- app/presenters/BasePresenter.php
+++ app/presenters/BasePresenter.php
@@ -5,11 +5,24 @@ namespace App\Presenters;
 use Nette,
 	App\Model;
 
-
 /**
  * Base presenter for all application presenters.
  */
 abstract class BasePresenter extends Nette\Application\UI\Presenter
 {
+
+	/**
+	 * backlink
+	 */
+	protected $backlink;
+
+	/**
+	 * Startup
+	 */
+	protected function startup()
+	{
+		parent::startup();
+		$this->template->backlink = $this->getParameter("backlink");
+	}
 
 }
