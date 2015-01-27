8bfe0f3746dc61063375cb2838c20253e325247f
literat <tomaslitera@hotmail.com>
2015-01-27 11:24:57 +0100

    Support for Metro UI fonts
      * add css/js to combined files
      * add jquery widget for menu support
      * fix checkbox issue

--- www/js/combined.js
+++ www/js/combined.js
@@ -1,3 +1,5 @@
 <!--#include file="jquery.js" -->
+<!--#include file="jquery.widget.min.js" -->
+<!--#include file="metro.min.js" -->
 <!--#include file="netteForms.js" -->
 <!--#include file="main.js" -->
