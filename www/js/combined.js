daf9eecd76ce848fca4b19ab0695ae9f7387a3e4
literat <tomas@litera.me>
2017-07-10 23:09:53 +0200

    refactor: Migrating framework to Laravel 5.4 (refs #25)

--- www/js/combined.js
+++
@@ -1,5 +0,0 @@
-<!--#include file="jquery.js" -->
-<!--#include file="jquery.widget.min.js" -->
-<!--#include file="metro.min.js" -->
-<!--#include file="netteForms.js" -->
-<!--#include file="main.js" -->
